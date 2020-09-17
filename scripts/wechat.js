e73882d5715060fb44ef5f64133906141820b815
cactusnix <cocalemontea@gmail.com>
2020-09-17 08:57:58 +0800

    format

--- scripts/wechat.js
+++ scripts/wechat.js
@@ -1,6 +1,6 @@
 // wechat
-var obj = JSON.parse($response.body);
-obj.advertisement_num = 0;
-obj.advertisement_info = [];
-delete obj.appid;
-$done({ body: JSON.stringify(obj) });
+var obj = JSON.parse($response.body)
+obj.advertisement_num = 0
+obj.advertisement_info = []
+delete obj.appid
+$done({ body: JSON.stringify(obj) })
