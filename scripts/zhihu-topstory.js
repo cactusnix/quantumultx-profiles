4ad468e4db4fafadb2fe5b7159facbeb6ef37d7d
cactusnix <cocalemontea@gmail.com>
2021-01-12 12:06:03 +0800

    test zhihu-topstory ad block

---
+++ scripts/zhihu-topstory.js
@@ -0,0 +1,12 @@
+// zhihu topstory
+let obj = JSON.parse($response.body)
+let indexList = []
+for (let i = 0; i < obj.data.length; i++) {
+  if (!obj.data[i].offset) {
+    indexList.push(i)
+  }
+}
+for (let i = 0; i < indexList.length; i++) {
+  obj.data.splice(indexList[i], 1)
+}
+$done({ body: JSON.stringify(obj) })
