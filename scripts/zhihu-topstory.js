422aa04aaa1b6eae0c90db42e9eac3644b109e6f
cactusnix <cocalemontea@gmail.com>
2021-01-12 12:27:35 +0800

    fix

--- scripts/zhihu-topstory.js
+++ scripts/zhihu-topstory.js
@@ -1,13 +1,8 @@
 // zhihu topstory
 let obj = JSON.parse($response.body)
-let indexList = []
 for (let i = 0; i < obj.data.length; i++) {
   if (!obj.data[i].offset) {
-    console.log(i)
-    indexList.push(i)
+    obj.data.splice(i, 1)
   }
-}
-for (let i = 0; i < indexList.length; i++) {
-  obj.data.splice(indexList[i], 1)
 }
 $done({ body: JSON.stringify(obj) })
