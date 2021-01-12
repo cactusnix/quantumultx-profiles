47dc416b9535c975f404e1eb6e5bf2874fe8fdcd
cactusnix <cocalemontea@gmail.com>
2021-01-12 12:23:59 +0800

    debug test

--- scripts/zhihu-topstory.js
+++ scripts/zhihu-topstory.js
@@ -3,11 +3,11 @@ let obj = JSON.parse($response.body)
 let indexList = []
 for (let i = 0; i < obj.data.length; i++) {
   if (!obj.data[i].offset) {
+    console.log(i)
     indexList.push(i)
   }
 }
 for (let i = 0; i < indexList.length; i++) {
   obj.data.splice(indexList[i], 1)
 }
-console.log(obj)
 $done({ body: JSON.stringify(obj) })
