671581bbb97a645ff559ef43b07f84e6188a195e
cactusnix <cocalemontea@gmail.com>
2021-03-12 15:26:23 +0800

    test 12306

--- scripts/12306.js
+++ scripts/12306.js
@@ -1,4 +1,5 @@
 // bilibili
 let obj = JSON.parse($response.body)
-obj.data.advertParam.skipTime = 0
+obj.data.advertParam.skipTime = 1000
+obj.data.advertParam.showSkipBtn = 0
 $done({ body: JSON.stringify(obj) })
