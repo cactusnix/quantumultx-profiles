d4660a56b7c3bac5a362c0991fe01529f52d4e6a
cactusnix <cocalemontea@gmail.com>
2021-04-01 16:43:41 +0800

    debug done

--- scripts/storeReqInfo.js
+++ scripts/storeReqInfo.js
@@ -15,19 +15,19 @@ if (url.indexOf("10010") > -1) {
       ";";
     console.log(cookieValue);
     if (cookieValue.indexOf("undefined") > -1) {
+      $notify(
+        "联通Cookie",
+        "操作结果",
+        "写入失败，请检测是否登录，并打开联通App！😬"
+      );
+      $done();
+    } else {
       $prefs.setValueForKey(cookieValue, "cookie_100010");
       $prefs.setValueForKey(
         "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148unicom",
         "user_agent_check"
       );
       $notify("联通Cookie", "操作结果", "写入成功🎉");
-      $done();
-    } else {
-      $notify(
-        "联通Cookie",
-        "操作结果",
-        "写入失败，请检测是否登录，并打开联通App！😬"
-      );
       $done();
     }
   }
