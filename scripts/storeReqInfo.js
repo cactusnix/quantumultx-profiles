fe1e0afa9a8db3fdec470a88b3aa387e7a0b13b2
cactusnix <cocalemontea@gmail.com>
2021-04-01 20:16:55 +0800

    fix bug

--- scripts/storeReqInfo.js
+++ scripts/storeReqInfo.js
@@ -5,37 +5,32 @@ const headers = $request.headers;
 if (url.indexOf("10010") > -1) {
   // simplify cookie
   const cookie = headers["Cookie"].split("; ");
-  const cookieValue =
-    cookie.find((v) => v.indexOf("ecs_token") > -1) +
-    ";" +
-    cookie.find((v) => v.indexOf("ecs_acc") > -1) +
-    ";";
+  const ecsToken = cookie.find((v) => v.indexOf("ecs_token") > -1) + ";";
+  const ecsAcc = cookie.find((v) => v.indexOf("ecs_acc") > -1) + ";";
   // validator the cookie
-  if (cookieValue.indexOf("undefined") > -1) {
+  // first check weather cookie is right
+  if (ecsToken.indexOf("undefined") > -1 || ecsAcc.indexOf("undefined") > -1) {
     $notify("中国联通Cookie获取失败（登录并打开联通App）😬", "", "");
     $done();
   } else {
-    if ($prefs.valueForKey("cookie_100010")) {
-      console.log("store get");
-      console.log($prefs.valueForKey("cookie_100010"));
-      console.log("cookie get");
-      console.log(cookieValue);
-      console.log("result");
-      console.log($prefs.valueForKey("cookie_100010") != cookieValue);
-      if ($prefs.valueForKey("cookie_100010") != cookieValue) {
-        $prefs.setValueForKey(cookieValue, "cookie_100010");
-        $notify("中国联通Cookie更新成功🎉", "", "");
+    if ($prefs.valueForKey("ecs_acc") && $prefs.valueForKey("ecs_token")) {
+      // only validator because ecs_token every time is different
+      if ($prefs.valueForKey("ecs_acc") != ecsAcc) {
+        $prefs.setValueForKey(ecsToken, "ecs_token");
+        $prefs.setValueForKey(ecsAcc, "ecs_acc");
+        $notify("中国联通ecs_acc更新成功🎉", "", "");
         $done();
       } else {
         $done();
       }
     } else {
-      $prefs.setValueForKey(cookieValue, "cookie_100010");
+      $prefs.setValueForKey(ecsToken, "ecs_token");
+      $prefs.setValueForKey(ecsAcc, "ecs_acc");
       $prefs.setValueForKey(
         "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148unicom",
         "user_agent_check"
       );
-      $notify("中国联通Cookie写入成功🎉", "", "");
+      $notify("中国联通Cookie首次写入成功🎉", "", "");
       $done();
     }
   }
