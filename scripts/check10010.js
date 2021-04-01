0f9806b4c153d808d0645fa6f31e109249ebf072
cactusnix <cocalemontea@gmail.com>
2021-04-01 17:36:31 +0800

    update

--- scripts/check10010.js
+++ scripts/check10010.js
@@ -1,44 +1,48 @@
 // this is for daily check
-const checkReq = {
-  url: "https://act.10010.com/SigninApp/signin/daySign",
-  method: "POST",
-  headers: {
-    Cookie: $prefs.valueForKey("cookie_100010"),
-    "User-Agent": $prefs.valueForKey("user_agent_check"),
-  },
-};
-const checkInfoReq = {
-  url: "https://act.10010.com/SigninApp/signin/getIntegral",
-  method: "POST",
-  headers: {
-    Cookie: $prefs.valueForKey("cookie_100010"),
-  },
-};
+// check action
+function check() {
+  return new Promise((resolve, reject) => {
+    const checkReq = {
+      url: "https://act.10010.com/SigninApp/signin/daySign",
+      method: "POST",
+      headers: {
+        Cookie: $prefs.valueForKey("cookie_100010"),
+        "User-Agent": $prefs.valueForKey("user_agent_check"),
+      },
+    };
+    $task.fetch(checkReq).then(
+      (response) => {
+        console.log("签到结果:", response.body.msg);
+      },
+      (reason) => {
+        // reason.error
+        console.log("签到Reason:", reason.error);
+      }
+    );
+  });
+}
+// get check info action
+function checkInfo() {
+  return new Promise((resolve, reject) => {
+    const checkInfoReq = {
+      url: "https://act.10010.com/SigninApp/signin/getIntegral",
+      method: "POST",
+      headers: {
+        Cookie: $prefs.valueForKey("cookie_100010"),
+      },
+    };
+    $task.fetch(checkInfoReq).then(
+      (response) => {
+        // response.statusCode, response.headers, response.body
+        console.log("目前积分", response.body.data.integralTotal);
+      },
+      (reason) => {
+        // reason.error
+        console.log("签到信息Reason:", reason.error);
+      }
+    );
+  });
+}
 
-$task.fetch(checkReq).then(
-  (response) => {
-    // response.statusCode, response.headers, response.body
-    console.log(response.body);
-    $notify("Title", "Subtitle", response.body); // Success!
-    $done();
-  },
-  (reason) => {
-    // reason.error
-    $notify("Title", "Subtitle", reason.error); // Error!
-    $done();
-  }
-);
-
-$task.fetch(checkInfoReq).then(
-  (response) => {
-    // response.statusCode, response.headers, response.body
-    console.log(response.body);
-    $notify("Title", "Subtitle", response.body); // Success!
-    $done();
-  },
-  (reason) => {
-    // reason.error
-    $notify("Title", "Subtitle", reason.error); // Error!
-    $done();
-  }
-);
+await check();
+await checkInfo();
