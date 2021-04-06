283fe34bce61a78caafbb428a89d6399809b4810
cactusnix <cocalemontea@gmail.com>
2021-04-06 11:20:52 +0800

    errro fix

--- backend/cactusnix.js
+++ backend/cactusnix.js
@@ -1,11 +1,22 @@
 // this backend is for get prefs data
-// eg: url: https://xxx.com?functionID="10010"
+// eg: url: https://xxx.com?functionID=10010
 const url = $request.url;
 
 const functionID = url.split("?")[1].split("=")[1];
 
-if (functionID === "10010") {
-  $done({
-    cookie: $prefs.valueForKey("cookie_10010"),
-  });
+switch (functionID) {
+  case "10010":
+    $done({
+      code: 0,
+      msg: "success",
+      cookie: $prefs.valueForKey("cookie_10010"),
+    });
+    break;
+
+  default:
+    $done({
+      code: 0,
+      msg: "default part",
+    });
+    break;
 }
