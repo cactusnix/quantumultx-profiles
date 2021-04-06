3cea1f5de5e729577a8865249494bee9ca8893db
cactusnix <cocalemontea@gmail.com>
2021-04-06 11:25:02 +0800

    debug

--- backend/cactusnix.js
+++ backend/cactusnix.js
@@ -1,15 +1,21 @@
 // this backend is for get prefs data
 // eg: url: https://xxx.com?functionID=10010
+const myStatus = "HTTP/1.1 200 OK";
 const url = $request.url;
 
 const functionID = url.split("?")[1].split("=")[1];
-
+console.log(url);
+console.log(functionID);
 switch (functionID) {
   case "10010":
     $done({
-      code: 0,
-      msg: "success",
-      cookie: $prefs.valueForKey("cookie_10010"),
+      status: myStatus,
+      headers: {},
+      body: {
+        code: 0,
+        msg: "success",
+        cookie: $prefs.valueForKey("cookie_10010"),
+      },
     });
     break;
 
