c3c908075eda50d81e86e6872c9a9f4774395221
cactusnix <cocalemontea@gmail.com>
2021-04-06 12:04:22 +0800

    debug

--- backend/cactusnix.js
+++ backend/cactusnix.js
@@ -5,37 +5,47 @@ const url = $request.url;
 const functionID = url.split("?")[1].split("=")[1];
 console.log(url);
 console.log(functionID);
+
 const myStatus = "HTTP/1.1 200 OK";
 const myHeaders = { Connection: "Close" };
-switch (functionID) {
-  case "10010":
-    $done({
-      status: myStatus,
-      headers: myHeaders,
-      body: {
-        code: 0,
-        msg: "success",
-        cookie: $prefs.valueForKey("cookie_10010"),
-      },
-    });
-    console.log({
-      status: myStatus,
-      headers: myHeaders,
-      body: {
-        code: 0,
-        msg: "success",
-        cookie: $prefs.valueForKey("cookie_10010"),
-      },
-    });
-    break;
+console.log({
+  status: myStatus,
+  headers: myHeaders,
+  body: {
+    code: 0,
+    msg: "success",
+    cookie: $prefs.valueForKey("cookie_10010"),
+  },
+});
+const myResponse = {
+  status: myStatus,
+  headers: myHeaders,
+  body: myData + JSON.stringify($request.headers),
+};
 
-  default:
-    const myResponse = {
-      status: myStatus,
-      headers: myHeaders,
-      body: myData + JSON.stringify($request.headers),
-    };
+$done(myResponse);
+// switch (functionID) {
+//   case "10010":
+//     $done({
+//       status: myStatus,
+//       headers: myHeaders,
+//       body: {
+//         code: 0,
+//         msg: "success",
+//         cookie: $prefs.valueForKey("cookie_10010"),
+//       },
+//     });
+//     console.log({
+//       status: myStatus,
+//       headers: myHeaders,
+//       body: {
+//         code: 0,
+//         msg: "success",
+//         cookie: $prefs.valueForKey("cookie_10010"),
+//       },
+//     });
+//     break;
 
-    $done(myResponse);
-    break;
-}
+//   default:
+//     break;
+// }
